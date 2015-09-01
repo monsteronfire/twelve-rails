3ba14b0133609fde1b253fd242da27f4474397ae
monsteronfire <monsteronfire@gmail.com>
2015-09-02 00:03:05 +0800

    adds user id to workouts

--- weightrack/db/schema.rb
+++ weightrack/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901000814) do
+ActiveRecord::Schema.define(version: 20150901142121) do
 
   create_table "exercises", force: :cascade do |t|
     t.string   "name"
@@ -48,6 +48,9 @@ ActiveRecord::Schema.define(version: 20150901000814) do
     t.string   "length"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "workouts", ["user_id"], name: "index_workouts_on_user_id"
 
 end
