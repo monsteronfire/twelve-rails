807ac6b3853635bb73f850572502e912c181dd12
monsteronfire <monsteronfire@gmail.com>
2015-09-05 10:41:58 +0800

    generates Note model

--- notebook/db/schema.rb
+++ notebook/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150905022850) do
+ActiveRecord::Schema.define(version: 20150905024059) do
+
+  create_table "notes", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
