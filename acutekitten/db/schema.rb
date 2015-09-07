b56bb9bfe7320c040d69628442a7fff2e17f015e
monsteronfire <monsteronfire@gmail.com>
2015-09-07 23:10:46 +0800

    adds refile for image attachment

--- acutekitten/db/schema.rb
+++ acutekitten/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907134905) do
+ActiveRecord::Schema.define(version: 20150907150449) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150907134905) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "image_id"
   end
 
   create_table "users", force: :cascade do |t|
