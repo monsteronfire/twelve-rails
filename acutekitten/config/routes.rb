b15a4c1803ff9f9dc1795c3991629a8599d2f8dd
monsteronfire <monsteronfire@gmail.com>
2015-09-08 23:36:57 +0800

    adds and configures acts_as_votable

--- acutekitten/config/routes.rb
+++ acutekitten/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :posts do
+    member do
+      get "like", to: "posts#upvote"
+      get "dislike", to: "posts#downvote"
+    end
     resources :comments
   end
   
