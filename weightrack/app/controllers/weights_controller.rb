b7947bbc2985eb5460a58abd5ed7b84b19c726e3
monsteronfire <monsteronfire@gmail.com>
2015-09-09 22:55:34 +0800

    starts attempt to implement weight into views

--- weightrack/app/controllers/weights_controller.rb
+++ weightrack/app/controllers/weights_controller.rb
@@ -1,5 +1,10 @@
 class WeightsController < ApplicationController
   def create
-    
+    @weight = current_user.weights.build(workout_params)
+    if @weight.save
+      redirect_to @weight, notice: "Successfully created new workout"
+    else
+      render 'new'
+    end
   end
 end
